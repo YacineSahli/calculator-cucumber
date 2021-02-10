d3054b221fe3f8657bef9ccdf7e5f3a5be94882f
yacinesahli <sahli.yacine@gmail.com>
2021-02-10 14:52:52 +0100

    #16: Fix: bug: integer division by zero

--- src/main/java/calculator/Divides.java
+++ src/main/java/calculator/Divides.java
@@ -2,21 +2,25 @@ package calculator;
 
 import java.util.List;
 
-final public class Divides extends Operation
-{
+final public class Divides extends Operation {
 
-  public /*constructor*/ Divides(List<Expression> elist) throws IllegalConstruction {
-	super(elist);
-	symbol = "/";
-	neutral = 1;
-	}
-
-  public Divides(List<Expression> elist, Notation n) throws IllegalConstruction {
-	super(elist,n);
-	symbol = "/";
-	neutral = 1;
+    public /*constructor*/ Divides(List<Expression> elist) throws IllegalConstruction {
+        super(elist);
+        symbol = "/";
+        neutral = 1;
     }
-  
-  public int op(int l, int r)
-    { return (l/r); }
+
+    public Divides(List<Expression> elist, Notation n) throws IllegalConstruction {
+        super(elist, n);
+        symbol = "/";
+        neutral = 1;
+    }
+
+    public int op(int l, int r) throws ArithmeticException {
+        if (r == 0) {
+            throw new ArithmeticException("Division per zero");
+        } else {
+            return (l / r);
+        }
+    }
 }
--- src/test/java/junit5tests/TestEvaluator.java
+++ src/test/java/junit5tests/TestEvaluator.java
@@ -14,7 +14,7 @@ public class TestEvaluator {
     @SuppressWarnings("unused")
     private Evaluator visitor;
     private Calculator calc;
-    private int value1, value2;
+    private int value1, value2, zero;
     private Expression op;
 
     @BeforeEach
@@ -23,6 +23,7 @@ public class TestEvaluator {
         calc = new Calculator();
         value1 = 8;
         value2 = 6;
+        zero = 0;
     }
 
     @Test
@@ -69,6 +70,16 @@ public class TestEvaluator {
         try { op = new Times(Arrays.asList(new MyNumber(value1), new MyNumber(value2)));
             assertEquals( value1 * value2,
                     calc.eval(op) );
+        }
+        catch(IllegalConstruction e) {
+            fail();
+        }
+    }
+
+    @Test
+    public void testEvaluatorDividesPerZero() {
+        try { op = new Divides(Arrays.asList(new MyNumber(value1), new MyNumber(zero)));
+            assertThrows(ArithmeticException.class, () -> calc.eval(op));
         }
         catch(IllegalConstruction e) {
             fail();
