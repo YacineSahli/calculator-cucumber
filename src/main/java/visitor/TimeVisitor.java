75cb852716721346a7ba3e227399cff72869ae19
yacinesahli <sahli.yacine@gmail.com>
2021-03-29 20:00:48 +0200

    Prepare merge develop

--- src/main/java/visitor/TimeVisitor.java
+++
@@ -1,9 +0,0 @@
-package visitor;
-
-import calculator.MyTime;
-import calculator.Operation;
-
-public abstract class TimeVisitor extends Visitor {
-    public abstract void visit(MyTime n);
-    public abstract void visit(Operation o) throws EvaluatorException;
-}
