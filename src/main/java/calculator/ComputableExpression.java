60d8bb7ffaddfee51ceafb852362a752b146c410
ArnaudPalgen <arnaud.palgen@student.umons.ac.be>
2021-04-16 16:52:37 +0200

    ComputableExression: split op into several functions

--- src/main/java/calculator/ComputableExpression.java
+++ src/main/java/calculator/ComputableExpression.java
@@ -16,37 +16,15 @@ import java.util.stream.Stream;
  * @author Arnaud.P
  */
 public abstract class ComputableExpression {
-    protected String symbol;
+    protected String symbol; //the symbol of the ComputableExpression
     protected String funcName; // the name of the function to apply
 
     public CalculatorVariable op(CalculatorVariable... args) throws InvocationTargetException, NoSuchMethodException {
+        try{
+            Method fun;
 
-        //retrieve the most accurate level. i.e. with the lowest accuracy value
-        CalculatorVariable most_accurate_item = Arrays.stream(args)
-                .max((o1, o2) -> o2.getAccuracyLevel() - o1.getAccuracyLevel())
-                .get();
-
-        Class targetClass = most_accurate_item.getClass(); // The class to which all arguments will be converted
-        String convert_fun_name = "to" + targetClass.getName()
-                .substring(targetClass.getName().lastIndexOf(".") + 1);
-
-        // convert all args to the targetClass
-        final Exception[] exception = new Exception[1];
-        try {
-            Stream<Object> streamConvertedArgs = Arrays.stream(args).map(i -> {
-                try {
-                    return i.getClass().getMethod(convert_fun_name).invoke(i);
-                } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
-                    exception[0] = e;
-                }
-                return null;
-            });
-            if (exception[0] != null)
-                throw exception[0];
-
-            Object[] convertedArgs = streamConvertedArgs.toArray();
-
-            Method fun = null;
+            Class targetClass = computeTargetClass(args); // get the class in which convert the arguments
+            Object[] convertedArgs = convertArgs(targetClass, args); // convert the args too this class
 
             // apply the function funcName.
             fun = this.getClass().getMethod(funcName, Collections.nCopies(args.length, targetClass)
@@ -57,9 +35,42 @@ public abstract class ComputableExpression {
         } catch (NoSuchMethodException e) {
             throw e;
         } catch (Exception error) {
-            error.printStackTrace();
+            error.printStackTrace();//todo throw error and catch error in main or CLI ?
         }
         return null;
+    }
+
+    private Class computeTargetClass(CalculatorVariable[] args){
+        //retrieve the most accurate level. i.e. with the lowest accuracy value
+        CalculatorVariable most_accurate_item = Arrays.stream(args)
+                .max((o1, o2) -> o2.getAccuracyLevel() - o1.getAccuracyLevel())
+                .get();
+
+        return most_accurate_item.getClass(); // The class to which all arguments will be converted
+    }
+
+    private Object[] convertArgs(Class targetClass, CalculatorVariable[] args) throws Exception {
+
+         /* compute the name of the convert function
+          * ex: if targetClass is calculator.variables.IntegerNumber
+          * the convert function's name is toIntegerNumber
+          */
+        String convert_fun_name = "to" + targetClass.getName()
+                .substring(targetClass.getName().lastIndexOf(".") + 1);
+
+        // convert all args to the targetClass
+        final Exception[] exception = new Exception[1];
+            Stream<Object> streamConvertedArgs = Arrays.stream(args).map(i -> {
+                try {
+                    return i.getClass().getMethod(convert_fun_name).invoke(i);
+                } catch (IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+                    exception[0] = e;
+                }
+                return null;
+            });
+            if (exception[0] != null)
+                throw exception[0];
+            return streamConvertedArgs.toArray();
     }
 
     final protected CalculatorVariable buildNumber(int num, int denum) {
