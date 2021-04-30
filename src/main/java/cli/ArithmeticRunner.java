e53c04178243cf1d148df737e37e27551b1c1283
Yacine <YacineSahli@users.noreply.github.com>
2021-04-30 19:41:49 +0200

    Develop (#22)
    Update from develop

--- src/main/java/cli/ArithmeticRunner.java
+++ src/main/java/cli/ArithmeticRunner.java
@@ -28,27 +28,6 @@ public class ArithmeticRunner extends Runner {
 
     @Override
     public String getHelp() {
-        return super.getHelp() +
-                "-------------------------------------------------- CALCULATOR HELP --------------------------------------------------\n\n" +
-                "This mode allows you to perform arithmetic operations.\n" +
-                "Expressions can be written in prefix, infix and postfix notation.\n\n" +
-                "In this mode, integers and rational numbers are supported: rational numbers are always noted as follows: 'r<integer>/<integer>'. For example: r5/2.\n\n" +
-                "The following operations are available:\n\n" +
-                "| Operation       |    Symbol    | Example (infix notation) |\n" +
-                "|:----------------|:------------:|:------------------------:|\n" +
-                "| PLUS            |     '+'      |           8+2            |\n" +
-                "| MINUS           |     '-'      |           5-9            |\n" +
-                "| TIMES           |     '*'      |           3*4            |\n" +
-                "| DIVISION        |     '/'      |           8/4            |\n" +
-                "| MODULAR INVERSE |   'modinv'   |       4 modinv 11        |\n" +
-                "| MODULO          | '%' or 'mod' |           5%2            |\n" +
-                "| POW             | '^' or 'pow' |           3^2            |\n\n" +
-                "The following functions are available.\n" +
-                "Note: the functions can only be used with the notation used in the example.\n\n" +
-                "| Function | Symbol | Example |\n" +
-                "|:---------|:------:|:-------:|\n" +
-                "| ABS      | 'abs'  | abs(-2) |\n" +
-                "| INV      | 'inv'  | inv(3)  |\n\n" +
-                "----------------------------------------------------------------------------------------------------------\n";
+        return super.getHelp() + "\n"  + loadHelpFile("ArithmeticHelper.txt");
     }
 }
--- src/main/java/cli/DateRunner.java
+++ src/main/java/cli/DateRunner.java
@@ -27,42 +27,6 @@ public class DateRunner extends Runner {
 
     @Override
     public String getHelp() {
-        return super.getHelp() + "--------------------------------------------- CONVERTOR HELP ------------------------------------------------\n\n" +
-                "This mode allows you to do addition and substraction operations on dates and duration.\n" +
-                "|:---------------------------------------------------------------------------------------:|\n" +
-                "| PLUS            |     '+'      |           2020-12-11 10:10:10 + 2020-12-11 10:10:10    |\n" +
-                "| MINUS           |     '-'      |           2020-12-11 10:10:10 - PT10H20M55S            |\n" +
-                "|-----------------------------------------------------------------------------------------|\n\n"+
-
-                "List of supported operations" +
-                "| DATE + DURATION| DATE - DURATION  | DATE - DATE |\n\n" +
-                "INFIX, PREFIX and POSTFIX notations are supported. Examples\n" +
-                "| +(DATE, DURATION)         |        DATE + DURATION       |         (DATE, DURATION)+    |\n" +
-                "The separator inside the parenthesis for the POSTFIX and PREFIX notation can either be a space or a comma\n" +
-                "The supported date format are as follow.\n" +
-                "z being a timezone like GMT, CET, GMT+8:30\n" +
-                "a is the AM/PM hour otherwise the date is considered 24h format\n" +
-                "|:----------------------------------------:|\n" +
-                "| yyyy-MM-dd HH:mm:ss z                    |\n" +
-                "| yyyy-MM-dd HH:mm z                       |\n" +
-                "| yyyy-MM-dd HH z                          |\n" +
-                "| yyyy-MM-dd z                             |\n" +
-                "| yyyy-MM-dd HH:mm:ss a z                  |\n" +
-                "| yyyy-MM-dd HH:mm a  z                    |\n" +
-                "| yyyy-MM-dd HH a z                        |\n" +
-                "| yyyy-MM-dd HH:mm                         |\n" +
-                "| yyyy-MM-dd HH                            |\n" +
-                "| yyyy-MM-dd                               |\n" +
-                "| yyyy-MM-dd hh:mm:ss a                    |\n" +
-                "| yyyy-MM-dd hh:mm a                       |\n" +
-                "| yyyy-MM-dd hh a                          |\n" +
-                "|------------------------------------------|\n\n"+
-
-        "The supported duration format is ISO-8601 with entire seconds. Examples.\n\n" +
-                "|:-------------------------------------------------:|\n" +
-                "| 1 day                         | P1D               |\n" +
-                "| 2 hours                       | PT2H              |\n" +
-                "| 2 days, 3 hours and 4 minutes | P2DT3H4M          |\n" +
-                "|---------------------------------------------------|\n\n";
+        return super.getHelp() + "\n" + loadHelpFile("DateHelper.txt");
     }
 }
